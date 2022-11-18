d451ab8bffca68770190cfda448f77e11b450eb7
osmanoic <115774883+osmanoic@users.noreply.github.com>
2022-11-18 17:33:38 +0400

    added all subjects

--- subjects.js
+++ subjects.js
@@ -2,10 +2,29 @@ let subjects = [
 
     {
         "id":1001,
-        "subject":Math,
-        "location":London,
-        "price":$100,
+        "subject":'Math',
+        "location":'London',
+        "price":'$100',
         "spaces":5
 
-    }
+    },
+
+    {
+        "id":1001,
+        "subject":'english',
+        "location":'London',
+        "price":'$100',
+        "spaces":5
+
+    },
+
+    {
+        "id":1001,
+        "subject":'science',
+        "location":'London',
+        "price":'$100',
+        "spaces":5
+
+    },
+
 ]
