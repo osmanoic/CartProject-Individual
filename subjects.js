9d3c20b9265632ab76044e13bdc3722b3371edff
osmanoic <115774883+osmanoic@users.noreply.github.com>
2022-11-23 17:18:27 +0400

    Sort option

--- subjects.js
+++ subjects.js
@@ -1,30 +1,111 @@
-let subjects = [
-
+let products = [
     {
-        "id":1001,
-        "subject":'Math',
-        "location":'London',
-        "price":'$100',
-        "spaces":5
-
+                    "id": 1001,
+                    "subject":'Math',
+                    "location": "London",
+                    "price": '£1000',
+                    "image": "images/math.png",
+                    "availableInventory": 5 ,
+                    
     },
 
-    {
-        "id":1001,
-        "subject":'english',
-        "location":'London',
-        "price":'$100',
-        "spaces":5
+     {               
+                     "id": 1002,
+                     "subject":'Art',
+                    "location": 'Oxford',
+                    "price": '£10',
+                    "image": "images/art.png",
+                    "availableInventory": 5 ,
+                    
+     },
 
-    },
+ {
+                    "id": 1003,
+                    "subject":'English',
+                    "location": 'York',
+                    "price": '£100',
+                    "image": "images/english.png",
+                    "availableInventory": 5 ,
+                    
+ },
 
-    {
-        "id":1001,
-        "subject":'science',
-        "location":'London',
-        "price":'$100',
-        "spaces":5
+{
 
-    },
+                     "id": 1004,
+                     "subject":'Chemistry',
+                    "location": 'London',
+                    "price": '£100',
+                    "image": "images/chemistry.png",
+                    "availableInventory": 5 ,
+                    
+},
 
-]
+{
+                    "id": 1005,
+                    "subject":'Physics',
+                    "location": 'Oxford',
+                    "price": '£100',
+                    "image": "images/physics.png",
+                    "availableInventory": 5 ,
+                   
+},
+
+
+                    {
+                        "id": 1006,
+                        "subject":'Biology',
+                        "location": 'York',
+                        "price": '£100',
+                        "image": "images/biology.png",
+                        "availableInventory": 5 ,
+                        
+                    },
+
+                    {               
+                        "id": 1007,
+                        "subject":'Music',
+                        "location": 'London',
+                        "price": '£100',
+                        "image": "images/music.png",
+                        "availableInventory": 5 ,
+                        
+                    },
+
+                    {
+                        "id": 1008,
+                        "subject":'Business',
+                        "location": 'York',
+                        "price": '£100',
+                        "image": "images/business.png",
+                        "availableInventory": 5 ,
+                        
+                    },
+
+                    {
+
+                        "id": 1009,
+                        "subject":'French',
+                        "location": 'Oxford',
+                        "price": '£100',
+                        "image": "images/french.png",
+                        "availableInventory": 5 ,
+                        
+                    },
+
+                    {
+                        "id": 1010,
+                        "subject":'Mechanics',
+                        "location": 'London',
+                        "price": '£100',
+                        "image": "images/mechanics.png",
+                        "availableInventory": 5 ,
+                        
+                    },
+
+
+
+
+
+                    ]
+                    
+                    
